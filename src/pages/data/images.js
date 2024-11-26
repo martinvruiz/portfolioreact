81554118416e66c4bf93f05422f2d3cc93a64158
¨martinvruiz¨ <martinvruiz10@gmail.com>
2024-11-26 11:44:08 -0300

    img fix

--- src/pages/data/images.js
+++ src/pages/data/images.js
@@ -1,7 +1,7 @@
-import tasktodo from "../../../public/images/proyects/tasktodo.jpg"
-import riverplate from "../../../public/images/proyects/riverplate.png"
-import logo from "../../../public/images/logo/LogoMVRT.png"
-import passcheck from "../../../public/images/proyects/PassCheck.png"
+import tasktodo from "../../pages/images/proyects/tasktodo.jpg"
+import riverplate from "../../pages/images/proyects/riverplate.png"
+import logo from "../../pages/images/logo/LogoMVRT.png"
+import passcheck from "../../pages/images/proyects/PassCheck.png"
 
 
 
