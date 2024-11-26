b5c09ee59a53b047b2ceba2f52c6cc7ab712470a
¨martinvruiz¨ <martinvruiz10@gmail.com>
2024-11-26 10:56:40 -0300

    added proyect passcheck

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -18,10 +18,24 @@ export const Projects = ()=>{
             <h3 className="lg:text-3xl font-semibold my-4">Mis proyectos</h3>
             <div ref={ref} className={`flex flex-col items-center transition-all duration-1000 transform ${
         inView ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-10'
-      }`}>
-                <ProyectContainer  link="https://tasktodo-d84gddque-martin-villalo-ruizs-projects.vercel.app/" title="Task ToDo" img={images[0]} text="El objetivo es ayudar a organizar y priorizar tareas, gracias a esto nace Task toDo. Hecho con JS. Se pueden agregar tareas, moverlas de columna y al finalizarlas esta la opcion de eliminarlas."/>
-                <ProyectContainer link="https://clubariverplate.netlify.app/" title="River Plate" img={images[1]}  text="Es una landing page acerca de el club el cual soy hincha, hecho con React. Tiene varias secciones como titulos o el cuerpo tecnico de la temporada actual."/>
-                <ProyectContainer title="Proximamente"  text="Proximamente..." img={images[2]}/>
+        }`}>
+                <ProyectContainer  
+                link="https://tasktodo-d84gddque-martin-villalo-ruizs-projects.vercel.app/" 
+                title="Task ToDo" 
+                img={images[0]} 
+                text="El objetivo es ayudar a organizar y priorizar tareas, gracias a esto nace Task toDo. Hecho con JS. Se pueden agregar tareas, moverlas de columna y al finalizarlas esta la opcion de eliminarlas."/>
+                <ProyectContainer link="https://clubariverplate.netlify.app/" 
+                title="River Plate" 
+                img={images[1]}  
+                text="Es una landing page acerca de el club el cual soy hincha, hecho con React. Tiene varias secciones como titulos o el cuerpo tecnico de la temporada actual."/>
+                <ProyectContainer link="https://passcheck-one.vercel.app/" 
+                title="Password Check" 
+                img={images[3]}  
+                text="Esta app, hecha con React, sirve para que pruebes tus contraseñás para ver que tan seguras son. Al ingresar tu contraseña se van marcando las combinaciones en verde cuando las cumplas, mientras mas tengas mas segura es.."/>
+                <ProyectContainer 
+                title="Proximamente"  
+                text="Proximamente..." 
+                img={images[2]}/>
             </div>
         </span>
     
--- src/pages/data/images.js
+++ src/pages/data/images.js
@@ -1,9 +1,10 @@
 import tasktodo from "../../images/proyects/tasktodo.jpg"
 import riverplate from "../../images/proyects/riverplate.png"
 import logo from "../../images/logo/LogoMVRT2-removebg-preview.png"
+import passcheck from "../../images/proyects/PassCheck.png"
 
 
 
-const images = [tasktodo, riverplate, logo]
+const images = [tasktodo, riverplate, logo, passcheck]
 
 export default images
